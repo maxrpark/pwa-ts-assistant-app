2e57bcc55e85c27861d3dce56d36b2adf0ddefa8
maxrpark <projectoindit@gmail.com>
2022-10-30 12:07:20 +0900

    export app

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,10 @@
+import { Request, Response } from "express";
 import express from "express";
 const app = express();
-app.get("/api", (req, res) => {
-    res.send("Express on Vercel");
+app.get("/api", (req: Request, res: Response) => {
+  res.json({ msg: "Express on Vercel" });
 });
-app.listen(5000, () => {
-    console.log("Running on port 5000.");
-});
-module.exports = app;
+// app.listen(5000, () => {
+//   console.log("Running on port 5000.");
+// });
+export default app;
