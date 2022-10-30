3c03acffd740ef62955b9dcfbb24ea2b60a5d79c
maxrpark <projectoindit@gmail.com>
2022-10-30 11:59:21 +0900

    appi rpute

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 const app = express();
 app.get("/api", (req, res) => {
-    res.send("Express on Vercel");
+  res.send("Express on Vercel");
 });
-app.listen(5000, () => {
-    console.log("Running on port 5000.");
-});
+// app.listen(5000, () => {
+//     console.log("Running on port 5000.");
+// });
 module.exports = app;
---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+app.get("/api", (req, res) => {
+    res.send("Express on Vercel");
+});
+app.listen(5000, () => {
+    console.log("Running on port 5000.");
+});
+module.exports = app;
