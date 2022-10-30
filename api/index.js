85b6ef228c93ae79915ef5cbbb8e291b3ad6eef4
maxrpark <projectoindit@gmail.com>
2022-10-30 12:25:57 +0900

    edits

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 const app = express();
 app.get("/api", (req, res) => {
-    res.json({ msg: "Express on Vercel" });
+    res.json({ data: [{ _id: 2123123, task: "make it work" }] });
 });
-// app.listen(5000, () => {
-//   console.log("Running on port 5000.");
-// });
+app.listen(5000, () => {
+    console.log("Running on port 5000.");
+});
 export default app;
