39ff098dfa40753ac3e8d082d404bc414d2d1736
maxrpark <projectoindit@gmail.com>
2022-10-30 11:32:00 +0900

    express server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,14 @@
 import { Request, Response, Application } from "express";
 import express from "express";
 
-// Initialize Express
 const app: Application = express();
 
-// Create GET request
 app.get("/", (req: Request, res: Response) => {
   res.send("Express on Vercel");
 });
 
-// Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
 
-// Export the Express API
 module.exports = app;
